0cdd6b7f848294cfd68ff8da14f93240df294bd9
Denis Granha <denisgranha@gmail.com>
2022-02-13 13:17:53 +0100

    first commit

---
+++ fluctua_nft_backend/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fluctua_nft_backend.users.models import User
+from fluctua_nft_backend.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
