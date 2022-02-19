b9f29f35631ba10bce641bd957a845bde2603ec6
Denis Granha <denisgranha@gmail.com>
2022-02-19 03:20:52 +0100

    linting

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path("users/", include("fluctua_nft_backend.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     path(
-        "api/v1/spotify/", include("fluctua_nft_backend.spotify.urls",
-        namespace="spotify"),
+        "api/v1/spotify/", 
+        include("fluctua_nft_backend.spotify.urls", namespace="spotify"),
     )
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
