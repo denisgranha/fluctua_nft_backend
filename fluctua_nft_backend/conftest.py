8d4da7537160ae293c84b26182853935fc22367b
Denis Granha <denisgranha@gmail.com>
2022-02-21 22:11:12 +0100

    remove old User model reference from conftest

--- fluctua_nft_backend/conftest.py
+++ fluctua_nft_backend/conftest.py
@@ -4,8 +4,3 @@ import pytest
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
